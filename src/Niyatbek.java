3a79ba134645833ce70a7b6151068a6bd5ba9a46
alimovny <alimovny04 @gmail.com>
2025-02-07 08:00:38 +0300

    class ny added

--- src/Niyatbek.java
+++ src/Niyatbek.java
@@ -1,11 +1,5 @@
 public class Niyatbek {
-    public static void main(String[] args) {
 
-        static void counter(){
-            for (int i = 0; i < 100; i++) {
-                System.out.println("it's - "+i);
-            }
-        }
+    public static void test(){}
 
-    }
 }
