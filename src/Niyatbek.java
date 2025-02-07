697dccefa0cd701e13ef090de51ffab64fb9fde5
alimovny <alimovny04 @gmail.com>
2025-02-07 08:31:35 +0300

    class ny added

--- src/Niyatbek.java
+++ src/Niyatbek.java
@@ -1,4 +1,9 @@
+import org.w3c.dom.ls.LSOutput;
+
 public class Niyatbek {
+    public static void main(String[] args) {
+        System.out.println("merhaba");
+    }
 
     public static void test(){}
 
