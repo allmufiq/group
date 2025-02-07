27933b2b6405f3e02dcdec590ddae45a63502d7c
alimovny <alimovny04 @gmail.com>
2025-02-07 08:38:23 +0300

    class ny updated

--- src/Niyatbek.java
+++ src/Niyatbek.java
@@ -3,6 +3,9 @@ import org.w3c.dom.ls.LSOutput;
 public class Niyatbek {
     public static void main(String[] args) {
         System.out.println("merhaba");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("hi");
+        }
     }
 
     public static void test(){}
